3220217a6524e48739ec047c61e8de061f32249a
@1.如既往 <zongshuren@163.com>
2021-07-10 18:20:35 +0800

    第一次提交

--- class_03/rock_scissors_paper.py
+++ class_03/rock_scissors_paper.py
@@ -14,7 +14,9 @@ class Rock_Scissors_paper():
     def get_role_name(self):
 
         role_num = input('选择角色1:曹操,2:刘备,3:诸葛亮')
-
+        if int(role_num) not in self.role_dict:
+            print('角色不存在，请重新选择')
+            self.get_role_name()
         return self.role_dict[int(role_num)]
 
     def get_role_fist(self):
@@ -31,13 +33,13 @@ class Rock_Scissors_paper():
 
     def game_begin(self):
         role_name = self.get_role_name()
-        role_quan = self.get_role_fist()
-        comp_name = self.get_computer_fist()
         print('角色为{0}'.format(role_name))
         role_win = 0
         comp_win = 0
         ping = 0
         while True:
+            role_quan = self.get_role_fist()
+            comp_name = self.get_computer_fist()
             if role_quan-comp_name == 1 or role_quan-comp_name == 2:
                 role_win += 1
                 print('{0}赢了'.format(role_name))
@@ -45,12 +47,13 @@ class Rock_Scissors_paper():
                 comp_win += 1
                 print('电脑赢了')
             elif role_quan-comp_name == 0:
+                print('平局')
                 ping += 1
-            jixu = input('按n退出游戏')
+            jixu = input('按n退出游戏,输入其他继续')
             if jixu == 'n':
-
                 break
+        print('游戏结束{0}赢了:{1}次,电脑赢了{2}次,平局{3}次'.format(role_name, role_win, comp_win, ping))
 
 if __name__ == '__main__':
 
-    print(Rock_Scissors_paper().game_begin())
+    Rock_Scissors_paper().game_begin()
