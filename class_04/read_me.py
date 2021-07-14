61dc8161023fbaec6c43fad467b168b1515f0661
@1.如既往 <zongshuren@163.com>
2021-07-14 22:31:10 +0800

    第一次提交

--- class_04/read_me.py
+++ class_04/read_me.py
@@ -13,11 +13,11 @@ class MathMethod:
         return self.a - self.b
 
 class MathMethod_1(MathMethod):
-    def divide(self): # 拓展
+    def divide(self):  # 拓展
         return self.a/self.b
 
     def add(self):
-        super(MathMethod_1, self).add()
+        super(MathMethod_1, self).add()  # 超继承
         print(self.a + self.b + 10)
 
 if __name__ == '__main__':
